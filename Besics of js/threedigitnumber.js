034d707e87658af61f0c31ff21e746285b7af96f
rohitkr963 <nk7565331@gmail.com>
2024-03-25 22:58:14 +0530

    three digit or not

--- Besics of js/threedigitnumber.js
+++ Besics of js/threedigitnumber.js
@@ -1,8 +1,8 @@
 const a= 101;
 if(a>99&&a<1000)
 {
-    console.log("it's three digit" )
+    console.log("it's three digit" );
 }
 else{
-    console.log("it's not three ditit")
+    console.log("it's not three ditit");
 }
