5e3edde83d3de8884021a6456e7873d859d01a80
rohitkr963 <nk7565331@gmail.com>
2024-03-26 15:53:04 +0530

    practice q1

---
+++ Array/practice1.js
@@ -0,0 +1,13 @@
+let item=[500,200,895,762,541,235];
+
+let i=0;
+
+for(let val of item)
+{
+    let offer=val/10;
+    item[i]=item[i]-offer;
+    console.log(`value after offer = ${item[i]}`);
+    i++;
+}
+
+
