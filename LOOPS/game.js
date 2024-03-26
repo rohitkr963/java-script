5787748123e7ea8f58a70602c5567a6348f21a60
rohitkr963 <nk7565331@gmail.com>
2024-03-26 14:46:38 +0530

    game

---
+++ LOOPS/game.js
@@ -0,0 +1,9 @@
+let gamenum=39;
+
+let username=promt("Guess the game number");
+while(gamenum!=username);
+{
+    username=promt("you entered wrong number Guess again");;
+
+}
+console.log("congratulation you entered right num");
