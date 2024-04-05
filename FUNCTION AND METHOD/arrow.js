38d3fc42652b47e32e35b73875c31a545062b9b0
rohitkr963 <nk7565331@gmail.com>
2024-04-05 17:16:33 +0530

    practice questions

---
+++ FUNCTION AND METHOD/arrow.js
@@ -0,0 +1,37 @@
+
+// add function
+function MyFunction(a,b)
+{
+    return a+b;
+}
+
+//    arrow sum                                                 //modern js
+
+const sum=(a,b) =>{
+    console.log(a+b);
+};
+
+  // sum(3,7);
+
+   // multiple function
+
+   function multi(a,b)
+{
+    return a*b;
+}
+
+// arrow multiply
+
+const mul=(a,b) =>
+{
+    console.log(a*b);
+};
+
+//we can use here return keyword
+
+const mul1=(a,b) =>
+{
+    return a*b;
+};
+
+ mul1(3,5);
