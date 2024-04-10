4931d1b09b8747cd242ab22c241879bb16b83f32
rohitkr963 <nk7565331@gmail.com>
2024-04-10 20:05:15 +0530

    concat

--- Array/array method/concat.js
+++ Array/array method/concat.js
@@ -2,4 +2,4 @@ let marvelHeroes=["ironman","thor","spiderman","dr.strange"];
 let dcHeroes=["superman","batman","flash"];
 
 let heroes = marvelHeroes.concat(dcHeroes);
-console.log(heroes);
+console.log(heroes)
